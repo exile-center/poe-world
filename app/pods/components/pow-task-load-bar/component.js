9676aab08a25f444e7bc21fc3a39fb0054568ecf
Pascal Boutin <pboutin@mirego.com>
2018-11-29 00:21:35 -0500

    Fix missing decorators

--- app/pods/components/pow-task-load-bar/component.js
+++ app/pods/components/pow-task-load-bar/component.js
@@ -1,6 +1,10 @@
 // Vendor
 import Component from '@ember/component';
+import {argument} from '@ember-decorators/argument';
+import {type} from '@ember-decorators/argument/type';
 
 export default class TaskLoadBar extends Component {
+  @argument
+  @type('object')
   task = null;
 }
