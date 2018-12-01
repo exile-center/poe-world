2a87533e0cfc74bfc21b043b24be50656237161c
Pascal Boutin <pboutin@mirego.com>
2018-12-01 18:57:53 -0500

    Update testem-electron config

--- testem-electron.js
+++ testem-electron.js
@@ -3,7 +3,7 @@ module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launchers: {
-    Electron: require('./lib/test-support/test-runner')
+    Electron: require('ember-electron/lib/test-support/test-runner')
   },
   launch_in_ci: [
     'Electron'
